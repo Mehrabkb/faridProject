8a145f2b75ba1a24ea1359dd9e24b05e5114bf60
mehrabkb <mkordbacheh75@gmail.com>
2026-05-12 13:46:20 +0330

    add show error in migrate php file

--- database/migrate.php
+++ database/migrate.php
@@ -1,5 +1,7 @@
 <?php
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 $config = require __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config.php';
 
